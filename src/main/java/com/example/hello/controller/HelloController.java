13978a121d51c069002e2e38935899b828c94c19
Uğur <37528419+grusker@users.noreply.github.com>
2026-03-24 21:55:44 +0100

    Update HelloController.java

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.hello.controller;
 
-import com.example.hello.service.GreetingService;
+import com.example.hello.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
